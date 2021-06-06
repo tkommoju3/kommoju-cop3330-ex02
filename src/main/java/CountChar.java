71bc9d37aa425e86fa09bf5a3caeef2113ba3afb
tanushka <tanushkakommoju@knights.ucf.edu>
2021-06-05 20:30:25 -0400

    second exercise

---
+++ src/main/java/CountChar.java
@@ -0,0 +1,20 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Tanushka Kommoju
+ */
+
+
+
+
+import java.util.Scanner;
+public class CountChar
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the input string? ");
+        String text = input.nextLine();
+        System.out.println(text + " has " + text.length() + " characters.");
+
+    }
+}
